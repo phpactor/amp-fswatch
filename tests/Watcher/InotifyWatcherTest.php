dc44d12bb979e47db410c4ef5ad2dd8f4bddb500
Daniel Leech <dan.t.leech@gmail.com>
2020-03-21 15:46:58 +0000

    Extracted watcher testcase

--- tests/Watcher/InotifyWatcherTest.php
+++ tests/Watcher/InotifyWatcherTest.php
@@ -2,103 +2,15 @@
 
 namespace Phpactor\AmpFsWatcher\Tests\Watcher;
 
-use Amp\Delayed;
-use Amp\Loop;
-use Closure;
-use Generator;
-use PHPUnit\Framework\Assert;
-use PHPUnit\Framework\TestCase;
-use Phpactor\AmpFsWatch\ModifiedFile;
-use Phpactor\AmpFsWatch\ModifiedFileBuilder;
 use Phpactor\AmpFsWatch\Watcher\InotifyWatcher;
-use Psr\Log\AbstractLogger;
-use Psr\Log\LoggerInterface;
-use Psr\Log\NullLogger;
-use Phpactor\AmpFsWatcher\Tests\IntegrationTestCase;
 
-class InotifyWatcherTest extends IntegrationTestCase
+class InotifyWatcherTest extends WatcherTestCase
 {
-    protected function setUp(): void
+    protected function createWatcher(): InotifyWatcher
     {
-        $this->workspace()->reset();
-    }
-
-    /**
-     * @dataProvider provideMonitors
-     */
-    public function testMonitors(Closure $plan, array $expectedModifications): void
-    {
-        $watcher = new InotifyWatcher(
+        return new InotifyWatcher(
             $this->workspace()->path(),
             $this->createLogger()
         );
-
-        $watcher->start();
-
-        $modifications = [];
-        $watcher->monitor(function (ModifiedFile $modification) use (&$modifications) {
-            $modifications[] = $modification;
-        });
-
-        Loop::run(function () use ($plan) {
-            $generator = $plan();
-            yield from $generator;
-            Loop::stop();
-        });
-
-        $this->assertEquals($expectedModifications, $modifications);
-    }
-
-    public function provideMonitors(): Generator
-    {
-        yield 'multiple single files' => [
-            function () {
-                yield new Delayed(10);
-                $this->workspace()->put('foobar', '');
-                $this->workspace()->put('foobar', '');
-                yield new Delayed(10);
-                $this->workspace()->put('foobar', '');
-                $this->workspace()->put('foobar', '');
-                yield new Delayed(10);
-            },
-            [
-                new ModifiedFile($this->workspace()->path('foobar'), ModifiedFile::TYPE_FILE),
-                new ModifiedFile($this->workspace()->path('foobar'), ModifiedFile::TYPE_FILE),
-                new ModifiedFile($this->workspace()->path('foobar'), ModifiedFile::TYPE_FILE),
-                new ModifiedFile($this->workspace()->path('foobar'), ModifiedFile::TYPE_FILE),
-            ]
-        ];
-
-        yield 'directories' => [
-            function () {
-                yield new Delayed(10);
-                mkdir($this->workspace()->path('barfoo'));
-                yield new Delayed(10);
-            },
-            [
-                new ModifiedFile($this->workspace()->path('barfoo'), ModifiedFile::TYPE_FOLDER),
-            ]
-        ];
-
-        yield 'file removal' => [
-            function () {
-                $this->workspace()->put('foobar', 'asd');
-                yield new Delayed(10);
-                unlink($this->workspace()->path('foobar'));
-                yield new Delayed(10);
-            },
-            [
-                new ModifiedFile($this->workspace()->path('foobar'), ModifiedFile::TYPE_FILE),
-            ]
-        ];
-    }
-
-    private function createLogger(): LoggerInterface
-    {
-        return new class extends AbstractLogger {
-            public function log($level, $message, array $context = array()) {
-                fwrite(STDERR, sprintf('[%s] %s', $level, $message)."\n");
-            }
-        };
     }
 }
